da3f9a846f5b6f154eb13adf882eb8d05ac2e089
Jose Carlos Rivera <jrivera4@uwo.ca>
2017-11-09 10:58:21 -0500

    Database class and methods placed in their own file

---
+++ database.C
@@ -0,0 +1,124 @@
+/*
+ * File: database.C
+ * Purpose: Contains methods to insert, remove and modify entries in a database
+ * Language: C++
+*/
+
+#include <math.h>
+#include <stdlib.h>
+#include <string>
+#include <Wt/Dbo/Dbo>
+#include <Wt/Dbo/backend/Sqlite3>
+#include "userAccount.C"
+
+using namespace std;
+namespace dbo = Wt::Dbo;
+
+class Database{
+
+public:
+
+
+void registerUser(string first, string last, string email, string password){
+
+  dbo::backend::Sqlite3 sqlite3("userAccounts.db");
+  dbo::Session session;
+  session.setConnection(sqlite3);
+  session.mapClass<User_Account>("user");
+  /*
+   * Will only use this when creating database for very first time
+   */
+  //session.createTables();
+  {
+    dbo::Transaction transaction(session);
+    User_Account *user = new User_Account();
+    user->setFirstName(first);
+    user->setLastName(last);
+    user->setEmail(email);
+    user->setPassword(password);
+    dbo::ptr<User_Account> userPtr = session.add(user);
+  }
+}
+
+
+
+//Method to query database account by email
+string getUserPassword(string input_email){
+  dbo::backend::Sqlite3 sqlite3("userAccounts.db");
+  dbo::Session session;
+  session.setConnection(sqlite3);
+  session.mapClass<User_Account>("user");
+ {
+    dbo::Transaction transaction(session);
+    dbo::ptr<User_Account> user_query = session.find<User_Account>().where("email = ?").bind(input_email);
+    return user_query->password;
+  }
+}
+
+//Method to erase a user from the database
+void deleteUser(string user_email){
+  dbo::backend::Sqlite3 sqlite3("userAccounts.db");
+  dbo::Session session;
+  session.setConnection(sqlite3);
+  session.mapClass<User_Account>("user");
+ {
+    dbo::Transaction transaction(session);
+    dbo::ptr<User_Account> trash = session.find<User_Account>().where("email = ?").bind(user_email);
+    if (trash)
+      trash.remove();
+  }
+}
+
+//Method to modify user's first name
+void modifyFirstName(string user_email, string new_name){
+  dbo::backend::Sqlite3 sqlite3("userAccounts.db");
+  dbo::Session session;
+  session.setConnection(sqlite3);
+  session.mapClass<User_Account>("user");
+ {
+    dbo::Transaction transaction(session);
+    dbo::ptr<User_Account> mod = session.find<User_Account>().where("email = ?").bind(user_email);
+    mod.modify()->first_name = new_name;
+ }
+}
+//Method to modify user's last name
+void modifyLastName(string user_email, string new_name){
+  dbo::backend::Sqlite3 sqlite3("userAccounts.db");
+  dbo::Session session;
+  session.setConnection(sqlite3);
+  session.mapClass<User_Account>("user");
+ {
+    dbo::Transaction transaction(session);
+    dbo::ptr<User_Account> mod = session.find<User_Account>().where("email = ?").bind(user_email);
+    mod.modify()->last_name = new_name;
+ }
+}
+
+//Method to modify user's email
+void modifyEmail(string user_email, string new_email){
+  dbo::backend::Sqlite3 sqlite3("userAccounts.db");
+  dbo::Session session;
+  session.setConnection(sqlite3);
+  session.mapClass<User_Account>("user");
+ {
+    dbo::Transaction transaction(session);
+    dbo::ptr<User_Account> mod = session.find<User_Account>().where("email = ?").bind(user_email);
+    mod.modify()->email = new_email;
+ }
+}
+
+//Method to modify user's password
+void modifyPassword(string user_email, string pass){
+  //Should call hash function before saving
+  dbo::backend::Sqlite3 sqlite3("userAccounts.db");
+  dbo::Session session;
+  session.setConnection(sqlite3);
+  session.mapClass<User_Account>("user");
+ {
+    dbo::Transaction transaction(session);
+    dbo::ptr<User_Account> mod = session.find<User_Account>().where("email = ?").bind(user_email);
+    mod.modify()->password = pass;
+ }
+}
+
+};
--- hueProject.C
+++ hueProject.C
@@ -10,24 +10,15 @@
 #include <Wt/WCssDecorationStyle>
 #include <Wt/WColor>
 #include <string>
-#include "userAccount.C"
+#include "database.C"
 
 using namespace Wt;
-namespace dbo = Wt::Dbo;
 
 
 class HueProject : public WApplication
 {
 public:
   HueProject(const WEnvironment& env);
-  
-  void registerUser(string first, string last, string email, string password);
-  string getUserPassword(string input_email);
-  void deleteUser(string user_email);
-  void modifyFirstName(string user_email, string new_name);
-  void modifyLastName(string user_email, string new_name);
-  void modifyEmail(string user_email, string new_email);
-  void modifyPassword(string user_email, string pass);
 
 private:
   WLineEdit *nameEdit_;
@@ -43,15 +34,11 @@ HueProject::HueProject(const WEnvironment& env)
 {
  useStyleSheet("css/main.css");
     root()->setStyleClass("main");
-    setTitle("Hue Project - Team 9");                               // application title
+    setTitle("Hue Project - Team 9");
 
-    //registerUser("Jose", "Rivera", "jrivera4@uwo.ca", "SeCrEt");
-    //string line = getUserPassword("jrivera4@uwo.ca");
-    //deleteUser("rsmith@uwo.ca");
-    //modifyFirstName("jrivera4@uwo.ca", "John");
-    //modifyLastName("jrivera4@uwo.ca", "Lazarus");
-    //modifyPassword("jrivera4@uwo.ca", "SECRET");
-    //modifyEmail("jrivera4@uwo.ca", "lays@fritolay.ca");
+    Database *session_database = new Database();
+
+    session_database->registerUser("Jose", "Rivera", "jrivera4@uwo.ca", "SeCrEt");
 
     //Header container and information
     WContainerWidget *header = new WContainerWidget();
@@ -127,110 +114,6 @@ HueProject::HueProject(const WEnvironment& env)
     buttonReg->setMargin(0, Left); 
 }
 
-////////////////////////////////////////DATABASE METHODS//////////////////////////////////////////
-
-//Method to create new user object and add them to userAccounts database
-void HueProject::registerUser(string first, string last, string email, string password){
-
-  dbo::backend::Sqlite3 sqlite3("userAccounts.db");
-  dbo::Session session;
-  session.setConnection(sqlite3);
-  session.mapClass<User_Account>("user");
-  /*
-   * Will only use this when creating database for very first time
-   */
-  session.createTables();
-  {
-    dbo::Transaction transaction(session);
-    User_Account *user = new User_Account();
-    user->setFirstName(first);
-    user->setLastName(last);
-    user->setEmail(email);
-    user->setPassword(password);
-    dbo::ptr<User_Account> userPtr = session.add(user);
-  }
-}
-
-
-
-//Method to query database account by email
-string HueProject::getUserPassword(string input_email){
-  dbo::backend::Sqlite3 sqlite3("userAccounts.db");
-  dbo::Session session;
-  session.setConnection(sqlite3);
-  session.mapClass<User_Account>("user");
- {
-    dbo::Transaction transaction(session);
-    dbo::ptr<User_Account> user_query = session.find<User_Account>().where("email = ?").bind(input_email);
-    return user_query->password;
-  }
-}
-
-//Method to erase a user from the database
-void HueProject::deleteUser(string user_email){
-  dbo::backend::Sqlite3 sqlite3("userAccounts.db");
-  dbo::Session session;
-  session.setConnection(sqlite3);
-  session.mapClass<User_Account>("user");
- {
-    dbo::Transaction transaction(session);
-    dbo::ptr<User_Account> trash = session.find<User_Account>().where("email = ?").bind(user_email);
-    if (trash)
-      trash.remove();
-  }
-}
-
-//Method to modify user's first name
-void HueProject::modifyFirstName(string user_email, string new_name){
-  dbo::backend::Sqlite3 sqlite3("userAccounts.db");
-  dbo::Session session;
-  session.setConnection(sqlite3);
-  session.mapClass<User_Account>("user");
- {
-    dbo::Transaction transaction(session);
-    dbo::ptr<User_Account> mod = session.find<User_Account>().where("email = ?").bind(user_email);
-    mod.modify()->first_name = new_name;
- }
-}
-//Method to modify user's last name
-void HueProject::modifyLastName(string user_email, string new_name){
-  dbo::backend::Sqlite3 sqlite3("userAccounts.db");
-  dbo::Session session;
-  session.setConnection(sqlite3);
-  session.mapClass<User_Account>("user");
- {
-    dbo::Transaction transaction(session);
-    dbo::ptr<User_Account> mod = session.find<User_Account>().where("email = ?").bind(user_email);
-    mod.modify()->last_name = new_name;
- }
-}
-
-//Method to modify user's email
-void HueProject::modifyEmail(string user_email, string new_email){
-  dbo::backend::Sqlite3 sqlite3("userAccounts.db");
-  dbo::Session session;
-  session.setConnection(sqlite3);
-  session.mapClass<User_Account>("user");
- {
-    dbo::Transaction transaction(session);
-    dbo::ptr<User_Account> mod = session.find<User_Account>().where("email = ?").bind(user_email);
-    mod.modify()->email = new_email;
- }
-}
-
-//Method to modify user's password
-void HueProject::modifyPassword(string user_email, string pass){
-  //Should call hash function before saving
-  dbo::backend::Sqlite3 sqlite3("userAccounts.db");
-  dbo::Session session;
-  session.setConnection(sqlite3);
-  session.mapClass<User_Account>("user");
- {
-    dbo::Transaction transaction(session);
-    dbo::ptr<User_Account> mod = session.find<User_Account>().where("email = ?").bind(user_email);
-    mod.modify()->password = pass;
- }
-}
 ////////////////////////////////////////////////MAIN APP//////////////////////////////////////////
 
 WApplication *createApplication(const WEnvironment& env)
